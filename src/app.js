65d962530d3cef18322a130f9474ace9fe77e7d5
Janice Lichtman <jalichtman@gmail.com>
2017-05-30 16:21:51 -0700

    added collection

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ import _ from 'underscore';
 import $ from 'jquery';
 import 'jquery-colpick';
 
+import Postit from 'models/postit';
+import PostitNotes from './collections/postit_notes.js';
+
+
+// don't use until $(docuent).ready
+var postitTemplate;
+var postitNotes;
 
 var postitData = [{
     text: "Backbone is a library not a Framework.",
@@ -12,8 +19,80 @@ var postitData = [{
     color: "#752310"
 }];
 
-$(document).ready(function() {
-  console.log("Lets go!");
+
+var myPostit = new Postit({
+  text: "This is a postit!",
+  color: "#752310"
 });
 
-// end
+
+var render =  function(postit){
+  var jsonPostit = postit.toJSON();
+  // invoke the compiled template to generate HTML using the data from a specific task
+  var generatedHTML = postitTemplate(jsonPostit);
+  $('#postits').append(generatedHTML);
+};
+
+
+var newPostItForm = function() {
+
+  // Get the values from the fields
+  var textData = $('#text').val();
+  $('#text').val('');
+
+  var colorData = $('#color').val();
+  $('#color').val('');
+
+  return {
+    text: textData,
+    color: colorData,
+  };
+};
+
+
+var renderNotes = function(postit_notes) {
+  // Clear the unordered list
+  $('#postits').empty();
+
+  // Iterate through the list rendering each Task
+  postitNotes.each(function(note) {
+    render(note);
+  });
+};
+
+
+
+$(document).ready(function() {
+
+  // compiling the template
+  postitTemplate = _.template($('#postit-template').html());
+
+  // render(myPostit);
+
+  // postitData.forEach(function(rawPostit){
+  //   var myPostit = new Postit(rawPostit);
+  //   render(myPostit);
+  // });
+
+
+  postitNotes = new PostitNotes(postitData);
+
+  renderNotes(postitNotes);
+
+  postitNotes.on("update", function() {
+    renderNotes(postitNotes);
+  });
+
+
+
+
+  $('.success').click(function(event){
+  var formData = newPostItForm();
+  var postit = new Postit(formData);
+
+  postitNotes.add(postit);
+  });
+
+
+
+});
